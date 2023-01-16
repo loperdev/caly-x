63a76ad40fe6b303e6e3b6a2c6359b511d549c31
dev vyas <devendravyas04@gmail.com>
2023-01-16 09:51:36 +0000

    Updating new changes

--- js/script.js
+++ js/script.js
@@ -18,6 +18,12 @@ $(document).ready(function() {
 	});
 	/* navigation */
 
+  /* audio-player-expand */
+  $(".common-audion-player .common-audion-player-detail .audio-icon").click(function(){
+    $(".common-audion-player .common-audion-player-detail").toggleClass("active");
+  });
+  /* audio-player-expand */
+
 	/* audio-player */
 	var playerTrack = $("#player-track"),
     bgArtwork = $("#bg-artwork"),
@@ -32,6 +38,8 @@ $(document).ready(function() {
     sHover = $("#s-hover"),
     playPauseButton = $("#play-pause-button"),
     i = playPauseButton.find("i"),
+    playIcon = $("#play-pause-button .fa-play"),
+    pauseIcon = $("#play-pause-button .fa-pause"),
     tProgress = $("#current-time"),
     tTime = $("#track-length"),
     seekT,
@@ -51,29 +59,54 @@ $(document).ready(function() {
     tFlag = false,
     albums = [
       "paint",
-	  "Me & You",
-      "Electro Boy",
-      "Home",
-      "Proxy (Original Mix)"
+      "paint",
+      "paint",
+      "paint",
+      "paint",
+	    // "Me & You",
+      // "Electro Boy",
+      // "Home",
+      // "Proxy (Original Mix)"
     ],
     trackNames = [
       "i dont like MONDAYS",
-	  "Alex Skrindo - Me & You",
-      "Kaaze - Electro Boy",
-      "Jordan Schor - Home",
-      "Martin Garrix - Proxy"
+      "i dont like MONDAYS",
+      "i dont like MONDAYS",
+      "i dont like MONDAYS",
+      "i dont like MONDAYS",
+	    // "Alex Skrindo - Me & You",
+      // "Kaaze - Electro Boy",
+      // "Jordan Schor - Home",
+      // "Martin Garrix - Proxy"
     ],
     albumArtworks = ["_1", "_2", "_3", "_4", "_5"],
     trackUrl = [
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/2.mp3",
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/1.mp3",
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/3.mp3",
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/4.mp3",
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/5.mp3"
+      // "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/2.mp3",
+      // "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/1.mp3",
+      // "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/3.mp3",
+      // "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/4.mp3",
+      // "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/5.mp3"
+      "audio/audio-1.mp3",
+      "audio/audio-2.mp3",
+      "audio/audio-3.mp3",
+      "audio/audio-4.mp3",
+      "audio/audio-5.mp3"
     ],
     playPreviousTrackButton = $("#play-previous"),
     playNextTrackButton = $("#play-next"),
     currIndex = -1;
+
+    // playPauseButton.on("click",function(){
+    //   if(albumArt.hasClass("active")) {
+    //     playIcon.removeClass("d-none");
+    //     pauseIcon.addClass("d-none");
+    //     alert("video play");
+    //   } else {
+    //     playIcon.addClass("d-none");
+    //     pauseIcon.removeClass("d-none");
+    //     alert("video pause");
+    //   }
+    // });
 
 function playPause() {
     setTimeout(function () {
@@ -82,6 +115,8 @@ function playPause() {
         albumArt.addClass("active");
         checkBuffering();
         i.attr("class", "fas fa-pause");
+        // playIcon.addClass("d-none");
+        // pauseIcon.removeClass("d-none");
         audio.play();
       } else {
         playerTrack.removeClass("active");
@@ -89,9 +124,13 @@ function playPause() {
         clearInterval(buffInterval);
         albumArt.removeClass("buffering");
         i.attr("class", "fas fa-play");
+        // playIcon.removeClass("d-none");
+        // pauseIcon.addClass("d-none");
         audio.pause();
       }
     }, 300);
+    $("#play-pause-button .fa-play").toggleClass("d-none");
+    $("#play-pause-button .fa-pause").toggleClass("d-none");
 }
 
 function showHover(event) {
@@ -195,7 +234,9 @@ function selectTrack(flag) {
     else --currIndex;
 
     if (currIndex > -1 && currIndex < albumArtworks.length) {
-      if (flag == 0) i.attr("class", "fa fa-play");
+      if (flag == 0) {
+        i.attr("class", "fa fa-play");
+      }
       else {
         albumArt.removeClass("buffering");
         i.attr("class", "fa fa-pause");
@@ -227,7 +268,8 @@ function selectTrack(flag) {
 
       albumName.text(currAlbum);
       trackName.text(currTrackName);
-      albumArt.find("img.active").removeClass("active");
+      // albumArt.find("img.active").removeClass("active");
+      albumArt.find(".image.active").removeClass("active");
       $("#" + currArtwork).addClass("active");
 
       bgArtworkUrl = $("#" + currArtwork).attr("src");
@@ -260,9 +302,13 @@ function initPlayer() {
 
     playPreviousTrackButton.on("click", function () {
       selectTrack(-1);
+      $("#play-pause-button .fa-play").addClass("d-none");
+      $("#play-pause-button .fa-pause").removeClass("d-none");
     });
     playNextTrackButton.on("click", function () {
       selectTrack(1);
+      $("#play-pause-button .fa-play").addClass("d-none");
+      $("#play-pause-button .fa-pause").removeClass("d-none");
     });
 }
 
