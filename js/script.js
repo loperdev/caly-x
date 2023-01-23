2ba7aa023d04c025748535591471480640769900
dev vyas <devendravyas04@gmail.com>
2023-01-23 12:53:25 +0000

    Updating changes mentioned by EL.S

--- js/script.js
+++ js/script.js
@@ -16,7 +16,63 @@ $(document).ready(function() {
 		$(".navbar-toggler").addClass("collapsed");
 		$(".navbar .navbar-collapse").removeClass("show");
 	});
+
 	/* navigation */
+
+  /* picture-gallery-carousel */
+  $(".picture-gallery-carousel").slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    asNavFor: '.picture-gallery-thumbnail-carousel',
+  });
+  /* picture-gallery-carousel */
+
+  /* picture-gallery-thumbnail-carousel */
+  $(".picture-gallery-thumbnail-carousel").slick({
+    dots: false,
+    arrows: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    // fade: true,
+    focusOnSelect: true,
+    vertical: true,
+    // verticalSwiping: true,
+    asNavFor: '.picture-gallery-carousel',
+    prevArrow: '<div class="slick-prev slick-arrow"><svg preserveAspectRatio="none" width="44" height="20" viewBox="0 0 44 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.977388 4.45085C-0.898554 3.01725 0.106506 0 2.45999 0H41.54C43.8843 0 44.8951 2.9979 43.037 4.43979L23.6461 19.4864C22.7689 20.1671 21.5487 20.1716 20.6666 19.4975L0.977388 4.45085Z" fill="#D9D9D9"/></svg></div>',
+    nextArrow: '<div class="slick-next slick-arrow"><svg preserveAspectRatio="none" width="44" height="20" viewBox="0 0 44 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.977388 4.45085C-0.898554 3.01725 0.106506 0 2.45999 0H41.54C43.8843 0 44.8951 2.9979 43.037 4.43979L23.6461 19.4864C22.7689 20.1671 21.5487 20.1716 20.6666 19.4975L0.977388 4.45085Z" fill="#D9D9D9"/></svg></div>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+          vertical: false,
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  /* picture-gallery-thumbnail-carousel */
 
   /* audio-player-expand */
   $(".common-audion-player .common-audion-player-detail .audio-icon").click(function(){
@@ -58,39 +114,44 @@ $(document).ready(function() {
     buffInterval = null,
     tFlag = false,
     albums = [
-      "paint",
-      "paint",
-      "paint",
-      "paint",
-      "paint",
+      "DJ Snake",
+      "Rick Ross",
+      "DJ Snake",
+      "Rick Ross",
+      "DJ Snake",
+      "DJ Snake",
 	    // "Me & You",
       // "Electro Boy",
       // "Home",
       // "Proxy (Original Mix)"
     ],
     trackNames = [
-      "i dont like MONDAYS",
-      "i dont like MONDAYS",
-      "i dont like MONDAYS",
-      "i dont like MONDAYS",
-      "i dont like MONDAYS",
+      "Run It",
+      "Taki Taki",
+      "Money In The Grave",
+      "Get Low",
+      "Purple Lamborghini",
+      "Let Me Love You",
+      "Run It",
 	    // "Alex Skrindo - Me & You",
       // "Kaaze - Electro Boy",
       // "Jordan Schor - Home",
       // "Martin Garrix - Proxy"
     ],
-    albumArtworks = ["_1", "_2", "_3", "_4", "_5"],
+    albumArtworks = ["_1", "_2", "_3", "_4", "_5", "_6"],
     trackUrl = [
       // "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/2.mp3",
       // "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/1.mp3",
       // "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/3.mp3",
       // "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/4.mp3",
       // "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/5.mp3"
-      "audio/audio-1.mp3",
-      "audio/audio-2.mp3",
-      "audio/audio-3.mp3",
-      "audio/audio-4.mp3",
-      "audio/audio-5.mp3"
+      "audio/DJ-Snake-Run-It.mp3",
+      "audio/DJ-Snake-Taki-Taki.mp3",
+      "audio/Rick-Ross-Money-In-The-Grave.mp3",
+      "audio/DJ-Snake-Get-Low.mp3",
+      "audio/Rick-Ross-Purple-Lamborghini.mp3",
+      "audio/DJ-Snake-Let-Me-Love-You.mp3",
+      "audio/DJ-Snake-Run-It.mp3",
     ],
     playPreviousTrackButton = $("#play-previous"),
     playNextTrackButton = $("#play-next"),
@@ -108,212 +169,225 @@ $(document).ready(function() {
     //   }
     // });
 
-function playPause() {
-    setTimeout(function () {
-      if (audio.paused) {
-        playerTrack.addClass("active");
-        albumArt.addClass("active");
-        checkBuffering();
-        i.attr("class", "fas fa-pause");
-        // playIcon.addClass("d-none");
-        // pauseIcon.removeClass("d-none");
-        audio.play();
-      } else {
-        playerTrack.removeClass("active");
-        albumArt.removeClass("active");
-        clearInterval(buffInterval);
-        albumArt.removeClass("buffering");
-        i.attr("class", "fas fa-play");
-        // playIcon.removeClass("d-none");
-        // pauseIcon.addClass("d-none");
-        audio.pause();
+  function playPause() {
+      setTimeout(function () {
+        if (audio.paused) {
+          playerTrack.addClass("active");
+          albumArt.addClass("active");
+          checkBuffering();
+          i.attr("class", "fas fa-pause");
+          // playIcon.addClass("d-none");
+          // pauseIcon.removeClass("d-none");
+          audio.play();
+        } else {
+          playerTrack.removeClass("active");
+          albumArt.removeClass("active");
+          clearInterval(buffInterval);
+          albumArt.removeClass("buffering");
+          i.attr("class", "fas fa-play");
+          // playIcon.removeClass("d-none");
+          // pauseIcon.addClass("d-none");
+          audio.pause();
+        }
+      }, 300);
+      $("#play-pause-button .fa-play").toggleClass("d-none");
+      $("#play-pause-button .fa-pause").toggleClass("d-none");
+  }
+
+  function showHover(event) {
+      seekBarPos = sArea.offset();
+      seekT = event.clientX - seekBarPos.left;
+      seekLoc = audio.duration * (seekT / sArea.outerWidth());
+
+      sHover.width(seekT);
+
+      cM = seekLoc / 60;
+
+      ctMinutes = Math.floor(cM);
+      ctSeconds = Math.floor(seekLoc - ctMinutes * 60);
+
+      if (ctMinutes < 0 || ctSeconds < 0) return;
+
+      if (ctMinutes < 0 || ctSeconds < 0) return;
+
+      if (ctMinutes < 10) ctMinutes = "0" + ctMinutes;
+      if (ctSeconds < 10) ctSeconds = "0" + ctSeconds;
+
+      if (isNaN(ctMinutes) || isNaN(ctSeconds)) insTime.text("--:--");
+      else insTime.text(ctMinutes + ":" + ctSeconds);
+
+      insTime.css({ left: seekT, "margin-left": "-21px" }).fadeIn(0);
+  }
+
+  function hideHover() {
+      sHover.width(0);
+      insTime.text("00:00").css({ left: "0px", "margin-left": "0px" }).fadeOut(0);
+  }
+
+  function playFromClickedPos() {
+      audio.currentTime = seekLoc;
+      seekBar.width(seekT);
+      hideHover();
+  }
+
+  function updateCurrTime() {
+      nTime = new Date();
+      nTime = nTime.getTime();
+
+      if (!tFlag) {
+        tFlag = true;
+        trackTime.addClass("active");
       }
-    }, 300);
-    $("#play-pause-button .fa-play").toggleClass("d-none");
-    $("#play-pause-button .fa-pause").toggleClass("d-none");
-}
 
-function showHover(event) {
-    seekBarPos = sArea.offset();
-    seekT = event.clientX - seekBarPos.left;
-    seekLoc = audio.duration * (seekT / sArea.outerWidth());
+      curMinutes = Math.floor(audio.currentTime / 60);
+      curSeconds = Math.floor(audio.currentTime - curMinutes * 60);
 
-    sHover.width(seekT);
+      durMinutes = Math.floor(audio.duration / 60);
+      durSeconds = Math.floor(audio.duration - durMinutes * 60);
 
-    cM = seekLoc / 60;
+      playProgress = (audio.currentTime / audio.duration) * 100;
 
-    ctMinutes = Math.floor(cM);
-    ctSeconds = Math.floor(seekLoc - ctMinutes * 60);
+      if (curMinutes < 10) curMinutes = "0" + curMinutes;
+      if (curSeconds < 10) curSeconds = "0" + curSeconds;
 
-    if (ctMinutes < 0 || ctSeconds < 0) return;
+      if (durMinutes < 10) durMinutes = "0" + durMinutes;
+      if (durSeconds < 10) durSeconds = "0" + durSeconds;
 
-    if (ctMinutes < 0 || ctSeconds < 0) return;
+      if (isNaN(curMinutes) || isNaN(curSeconds)) tProgress.text("00:00");
+      else tProgress.text(curMinutes + ":" + curSeconds);
 
-    if (ctMinutes < 10) ctMinutes = "0" + ctMinutes;
-    if (ctSeconds < 10) ctSeconds = "0" + ctSeconds;
+      if (isNaN(durMinutes) || isNaN(durSeconds)) tTime.text("00:00");
+      else tTime.text(durMinutes + ":" + durSeconds);
 
-    if (isNaN(ctMinutes) || isNaN(ctSeconds)) insTime.text("--:--");
-    else insTime.text(ctMinutes + ":" + ctSeconds);
+      if (
+        isNaN(curMinutes) ||
+        isNaN(curSeconds) ||
+        isNaN(durMinutes) ||
+        isNaN(durSeconds)
+      )
+        trackTime.removeClass("active");
+      else trackTime.addClass("active");
 
-    insTime.css({ left: seekT, "margin-left": "-21px" }).fadeIn(0);
-}
+      seekBar.width(playProgress + "%");
 
-function hideHover() {
-    sHover.width(0);
-    insTime.text("00:00").css({ left: "0px", "margin-left": "0px" }).fadeOut(0);
-}
-
-function playFromClickedPos() {
-    audio.currentTime = seekLoc;
-    seekBar.width(seekT);
-    hideHover();
-}
-
-function updateCurrTime() {
-    nTime = new Date();
-    nTime = nTime.getTime();
-
-    if (!tFlag) {
-      tFlag = true;
-      trackTime.addClass("active");
-    }
-
-    curMinutes = Math.floor(audio.currentTime / 60);
-    curSeconds = Math.floor(audio.currentTime - curMinutes * 60);
-
-    durMinutes = Math.floor(audio.duration / 60);
-    durSeconds = Math.floor(audio.duration - durMinutes * 60);
-
-    playProgress = (audio.currentTime / audio.duration) * 100;
-
-    if (curMinutes < 10) curMinutes = "0" + curMinutes;
-    if (curSeconds < 10) curSeconds = "0" + curSeconds;
-
-    if (durMinutes < 10) durMinutes = "0" + durMinutes;
-    if (durSeconds < 10) durSeconds = "0" + durSeconds;
-
-    if (isNaN(curMinutes) || isNaN(curSeconds)) tProgress.text("00:00");
-    else tProgress.text(curMinutes + ":" + curSeconds);
-
-    if (isNaN(durMinutes) || isNaN(durSeconds)) tTime.text("00:00");
-    else tTime.text(durMinutes + ":" + durSeconds);
-
-    if (
-      isNaN(curMinutes) ||
-      isNaN(curSeconds) ||
-      isNaN(durMinutes) ||
-      isNaN(durSeconds)
-    )
-      trackTime.removeClass("active");
-    else trackTime.addClass("active");
-
-    seekBar.width(playProgress + "%");
-
-    if (playProgress == 100) {
-      i.attr("class", "fa fa-play");
-      seekBar.width(0);
-      tProgress.text("00:00");
-      albumArt.removeClass("buffering").removeClass("active");
-      clearInterval(buffInterval);
-    }
-}
-
-function checkBuffering() {
-    clearInterval(buffInterval);
-    buffInterval = setInterval(function () {
-      if (nTime == 0 || bTime - nTime > 1000) albumArt.addClass("buffering");
-      else albumArt.removeClass("buffering");
-
-      bTime = new Date();
-      bTime = bTime.getTime();
-    }, 100);
-}
-
-function selectTrack(flag) {
-    if (flag == 0 || flag == 1) ++currIndex;
-    else --currIndex;
-
-    if (currIndex > -1 && currIndex < albumArtworks.length) {
-      if (flag == 0) {
+      if (playProgress == 100) {
         i.attr("class", "fa fa-play");
-      }
-      else {
-        albumArt.removeClass("buffering");
-        i.attr("class", "fa fa-pause");
-      }
-
-      seekBar.width(0);
-      trackTime.removeClass("active");
-      tProgress.text("00:00");
-      tTime.text("00:00");
-
-      currAlbum = albums[currIndex];
-      currTrackName = trackNames[currIndex];
-      currArtwork = albumArtworks[currIndex];
-
-      audio.src = trackUrl[currIndex];
-
-      nTime = 0;
-      bTime = new Date();
-      bTime = bTime.getTime();
-
-      if (flag != 0) {
-        audio.play();
-        playerTrack.addClass("active");
-        albumArt.addClass("active");
-
+        seekBar.width(0);
+        tProgress.text("00:00");
+        albumArt.removeClass("buffering").removeClass("active");
         clearInterval(buffInterval);
-        checkBuffering();
       }
+  }
 
-      albumName.text(currAlbum);
-      trackName.text(currTrackName);
-      // albumArt.find("img.active").removeClass("active");
-      albumArt.find(".image.active").removeClass("active");
-      $("#" + currArtwork).addClass("active");
+  function checkBuffering() {
+      clearInterval(buffInterval);
+      buffInterval = setInterval(function () {
+        if (nTime == 0 || bTime - nTime > 1000) albumArt.addClass("buffering");
+        else albumArt.removeClass("buffering");
 
-      bgArtworkUrl = $("#" + currArtwork).attr("src");
+        bTime = new Date();
+        bTime = bTime.getTime();
+      }, 100);
+  }
 
-      bgArtwork.css({ "background-image": "url(" + bgArtworkUrl + ")" });
-    } else {
-      if (flag == 0 || flag == 1) --currIndex;
-      else ++currIndex;
-    }
-}
+  function selectTrack(flag) {
+      if (flag == 0 || flag == 1) ++currIndex;
+      else --currIndex;
 
-function initPlayer() {
-    audio = new Audio();
+      if (currIndex > -1 && currIndex < albumArtworks.length) {
+        if (flag == 0) {
+          i.attr("class", "fa fa-play");
+        }
+        else {
+          albumArt.removeClass("buffering");
+          i.attr("class", "fa fa-pause");
+        }
 
-    selectTrack(0);
+        seekBar.width(0);
+        trackTime.removeClass("active");
+        tProgress.text("00:00");
+        tTime.text("00:00");
 
-    audio.loop = false;
+        currAlbum = albums[currIndex];
+        currTrackName = trackNames[currIndex];
+        currArtwork = albumArtworks[currIndex];
 
-    playPauseButton.on("click", playPause);
+        audio.src = trackUrl[currIndex];
 
-    sArea.mousemove(function (event) {
-      showHover(event);
-    });
+        nTime = 0;
+        bTime = new Date();
+        bTime = bTime.getTime();
 
-    sArea.mouseout(hideHover);
+        if (flag != 0) {
+          audio.play();
+          playerTrack.addClass("active");
+          albumArt.addClass("active");
 
-    sArea.on("click", playFromClickedPos);
+          clearInterval(buffInterval);
+          checkBuffering();
+        }
 
-    $(audio).on("timeupdate", updateCurrTime);
+        albumName.text(currAlbum);
+        trackName.text(currTrackName);
+        // albumArt.find("img.active").removeClass("active");
+        albumArt.find(".image.active").removeClass("active");
+        $("#" + currArtwork).addClass("active");
 
-    playPreviousTrackButton.on("click", function () {
-      selectTrack(-1);
-      $("#play-pause-button .fa-play").addClass("d-none");
-      $("#play-pause-button .fa-pause").removeClass("d-none");
-    });
-    playNextTrackButton.on("click", function () {
-      selectTrack(1);
-      $("#play-pause-button .fa-play").addClass("d-none");
-      $("#play-pause-button .fa-pause").removeClass("d-none");
-    });
-}
+        bgArtworkUrl = $("#" + currArtwork).attr("src");
 
-initPlayer();
+        bgArtwork.css({ "background-image": "url(" + bgArtworkUrl + ")" });
+      } else {
+        if (flag == 0 || flag == 1) --currIndex;
+        else ++currIndex;
+      }
+  }
+
+  function initPlayer() {
+      audio = new Audio();
+
+      selectTrack(0);
+
+      audio.loop = false;
+
+      playPauseButton.on("click", playPause);
+
+      sArea.mousemove(function (event) {
+        showHover(event);
+      });
+
+      sArea.mouseout(hideHover);
+
+      sArea.on("click", playFromClickedPos);
+
+      $(audio).on("timeupdate", updateCurrTime);
+
+      playPreviousTrackButton.on("click", function () {
+        selectTrack(-1);
+        $("#play-pause-button .fa-play").addClass("d-none");
+        $("#play-pause-button .fa-pause").removeClass("d-none");
+      });
+      playNextTrackButton.on("click", function () {
+        selectTrack(1);
+        $("#play-pause-button .fa-play").addClass("d-none");
+        $("#play-pause-button .fa-pause").removeClass("d-none");
+      });
+  }
+
+  initPlayer();
 	/* audio-player */
+
+  /* fancybox */
+  Fancybox.bind("[data-fancybox]", {
+    Thumbs: {
+      autoStart: false,
+    },
+		Toolbar: {
+		  display: [
+			"close",
+		  ],
+		},
+	});
+  /* fancybox */
 
 	// OFI Browser
 	objectFitImages();
